de064202ca604521d9cc6a3ecacd645fa79ea749
rsnyder <rsnyder@rsnyder-Ubuntu.(none)>
2013-03-05 17:37:17 -0500

    fixed static files

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from mysite import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,3 +8,8 @@ urlpatterns = patterns('',
     url(r'^tweeeter/', include('tweeeter.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('', (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+                                 {'document_root':'/home/rsnyder/helloWorld/mysite/mysite/media', 'show_indexes': True}), )
